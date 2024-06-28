4b5ebb7f38d3453fcd6f27a250178e929a4f35d9
TimeEntropy <3454493317@qq.com>
2024-06-28 16:21:32 +0800

    fix undo logic

--- scene/sandbox.gd
+++ scene/sandbox.gd
@@ -63,7 +63,7 @@ func replay() -> void:
 	board.player_changed.connect(_on_border_player_changed)
 	board.duel_win.connect(_on_duel_win)
 	board.duel_draw.connect(_on_duel_draw)
-	button_undo.pressed.connect(board.history.undo)
+	button_undo.pressed.connect(undo)
 	info_PC.nickname = board.players[0].nickname
 	info_ME.nickname = board.players[1].nickname
 	info_PC.image.texture = board.players[0].used_chess_image
@@ -74,6 +74,11 @@ func replay() -> void:
 
 func giveup() -> void:
 	board.let_win(0)
+	pass
+
+func undo() -> void:
+	board.history.undo()
+	win_prompt.hide()
 	pass
 
 #endregion
